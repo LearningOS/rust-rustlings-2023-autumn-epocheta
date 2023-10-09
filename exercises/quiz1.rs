9fb038335bd79a453ed6ec950f1fc9d50af0c1fa
epoche <tniasu@qq.com>
2023-10-09 09:55:22 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(num: i32) -> i32 {
+    match num {
+        x if x <= 40 => 2 * num,
+        _ => num
+    }
+}
 
 // Don't modify this function!
 #[test]
